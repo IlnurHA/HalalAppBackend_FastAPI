8becbd3b83d2fc8edcbcddf2be25d13b2c332139
IlnurHA <53339563+IlnurHA@users.noreply.github.com>
2024-01-03 18:39:33 +0300

    Address Info Into Tables

--- domain/models_sqlalchemy.py
+++ domain/models_sqlalchemy.py
@@ -2,6 +2,8 @@ from sqlalchemy import Column, Integer, String, ForeignKey, Text, Enum
 from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.ext.declarative import declarative_base
 
+from typing import List
+
 from domain.enums import Permissiveness
 
 
@@ -29,3 +31,30 @@ class IngredientsInfo(BaseModel):
     permissiveness: Mapped[Enum[Permissiveness]] = mapped_column()
     description: Mapped[Text] = mapped_column()
     img_src: Mapped[Text] = mapped_column(nullable=True)
+
+
+class FederationEntity(BaseModel):
+    __tablename__ = "federationEntities"
+
+    name: Mapped[Text] = mapped_column()
+
+    districts: Mapped[List["DistrictEntity"]] = relationship(back_populates="federation_entity")
+
+
+class DistrictEntity(BaseModel):
+    __tablename__ = "districtEntities"
+
+    name: Mapped[Text] = mapped_column()
+    federation_entity_fk: Mapped[int] = mapped_column(ForeignKey("FederationEntity.id", ondelete="CASCADE"))
+
+    federation_entity: Mapped["FederationEntity"] = relationship(back_populates="districts")
+    settlements: Mapped[List["DistrictSettlementEntity"]] = relationship(back_populates="district_entity")
+
+
+class DistrictSettlementEntity(BaseModel):
+    __tablename__ = "districtSettlementEntities"
+
+    name: Mapped[Text] = mapped_column()
+    district_entity_fk: Mapped[int] = mapped_column(ForeignKey("DistrictEntity.id", ondelete="CASCADE"))
+
+    district_entity: Mapped["DistrictEntity"] = relationship(back_populates="settlements")
