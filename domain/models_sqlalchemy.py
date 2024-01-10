433cb2de5e1ec583f9bf4e7735760fca0a962efb
IlnurHA <53339563+IlnurHA@users.noreply.github.com>
2024-01-10 20:15:41 +0300

    rebuilt sql tables: federation, district, and district settlement don't have ids

--- domain/models_sqlalchemy.py
+++ domain/models_sqlalchemy.py
@@ -15,6 +15,10 @@ class BaseModel(DeclarativeBase):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
 
 
+class BaseForFoodPointSchema(DeclarativeBase):
+    __abstract__ = True
+
+
 class FoodAdditive(BaseModel):
     __tablename__ = "foodAdditives"
 
@@ -35,47 +39,45 @@ class IngredientsInfo(BaseModel):
     img_src: Mapped[str] = mapped_column(nullable=True)
 
 
-class FederationEntity(BaseModel):
+class FederationEntity(BaseForFoodPointSchema):
     __tablename__ = "federationEntities"
 
-    name: Mapped[str] = mapped_column()
-
-    districts: Mapped[List["DistrictEntity"]] = relationship(back_populates="federation_entity")
+    name: Mapped[str] = mapped_column(primary_key=True)
 
 
-class DistrictEntity(BaseModel):
+class DistrictEntity(BaseForFoodPointSchema):
     __tablename__ = "districtEntities"
 
-    name: Mapped[str] = mapped_column()
-    federation_entity_fk: Mapped[int] = mapped_column(ForeignKey("federationEntities.id", ondelete="CASCADE"))
+    name: Mapped[str] = mapped_column(primary_key=True)
+    federation_name: Mapped[str] = mapped_column(ForeignKey("federationEntities.name", ondelete="CASCADE"))
 
-    federation_entity: Mapped["FederationEntity"] = relationship(back_populates="districts")
-    settlements: Mapped[List["DistrictSettlementEntity"]] = relationship(back_populates="district_entity")
+    federation_entity: Mapped["FederationEntity"] = relationship()
 
 
-class DistrictSettlementEntity(BaseModel):
+class DistrictSettlementEntity(BaseForFoodPointSchema):
     __tablename__ = "districtSettlementEntities"
 
-    name: Mapped[str] = mapped_column()
-    district_entity_fk: Mapped[int] = mapped_column(ForeignKey("districtEntities.id", ondelete="CASCADE"))
+    name: Mapped[str] = mapped_column(primary_key=True)
+    district_name: Mapped[str] = mapped_column(ForeignKey("districtEntities.name", ondelete="CASCADE"))
 
-    district_entity: Mapped["DistrictEntity"] = relationship(back_populates="settlements")
-
-    food_points: Mapped[List["FoodPoint"]] = relationship(back_populates="district_settlement")
+    district_entity: Mapped["DistrictEntity"] = relationship()
 
 
-class FoodPoint(BaseModel):
+class FoodPoint(BaseForFoodPointSchema):
     __tablename__ = "foodPoints"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
 
     name: Mapped[str] = mapped_column()
     is_prayer_room_exists: Mapped[bool] = mapped_column()
     food_point_type = mapped_column(Enum(FoodPointTypes), nullable=False)
     cuisine_type = mapped_column(Enum(CuisineTypes), nullable=False)
 
-    district_settlement_fk: Mapped[int] = mapped_column(ForeignKey("districtSettlementEntities.id", ondelete="CASCADE"))
+    district_settlement_name: Mapped[str] = mapped_column(
+        ForeignKey("districtSettlementEntities.name", ondelete="CASCADE"))
     street: Mapped[str] = mapped_column()
     building: Mapped[str] = mapped_column()
     halal_certificate_expiration_date = mapped_column(Date, nullable=False)
     img_src: Mapped[str] = mapped_column()
 
-    district_settlement: Mapped["DistrictSettlementEntity"] = relationship(back_populates="food_points")
+    district_settlement: Mapped["DistrictSettlementEntity"] = relationship()
